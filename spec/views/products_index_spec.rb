5e43446d8b6fc47a25b465a991d180c2c718ad4d
Ariel Zhen <zhenpingping@live.com>
2018-10-04 05:23:45 +0000

    automatically backed up by learn

--- spec/views/products_index_spec.rb
+++ spec/views/products_index_spec.rb
@@ -1,3 +1,4 @@
+
 require 'rails_helper'
 
 RSpec.describe "products/index", :type => :view do
@@ -12,7 +13,7 @@ RSpec.describe "products/index", :type => :view do
   end
 
   it "shows everything in the cart" do
-    assign(:cart, ['apples', 'bananas', 'pears'])
+    session[:cart] = ['apples', 'bananas', 'pears']
     render
     expect(rendered).to include 'apples'
     expect(rendered).to include 'bananas'
