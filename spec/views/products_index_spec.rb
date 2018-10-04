52617494a8c3c3c4c8f00506791686f812ecc8f4
Ariel Zhen <zhenpingping@live.com>
2018-10-04 05:25:54 +0000

    automatically backed up by learn

--- spec/views/products_index_spec.rb
+++ spec/views/products_index_spec.rb
@@ -19,23 +19,3 @@ RSpec.describe "products/index", :type => :view do
     expect(rendered).to include 'pears'
   end
 end
-
-RSpec.describe "products/index", :type => :view do
-  before do
-    controller.singleton_class.class_eval do
-      protected
-        def cart
-          session[:cart]
-        end
-        helper_method :cart
-    end
-  end
-
-  it "shows everything in the cart" do
-    assign(:cart, ['apples', 'bananas', 'pears'])
-    render
-    expect(rendered).to include 'apples'
-    expect(rendered).to include 'bananas'
-    expect(rendered).to include 'pears'
-  end
-end
